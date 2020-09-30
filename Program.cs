d65b70aec0d1c898a0400ab7c22ee249ff5da731
Evgh <kasperovich.official@gmail.com>
2020-09-30 14:45:18 +0300

    first change

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace oop_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Git World!");
         }
     }
 }
