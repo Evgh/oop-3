27d9bb0ab9ebbd731f31f4d93d2cd17167987803
Evgh <kasperovich.official@gmail.com>
2020-09-30 15:13:13 +0300

    change via visual studio

--- Program.cs
+++ Program.cs
@@ -2,11 +2,41 @@
 
 namespace oop_3
 {
-    class Program
+
+    class Abiturient
     {
+        string name;
+        string surname;
+        string father;
+        string adress;
+        string phone;
+
+
+        public int a;
+
+
+        Abiturient()
+        {
+            a = 0;
+        }
+        Abiturient(int x)
+        {
+            a = x > 0 && x <= 10 ? x : 0;
+        }
+        Abiturient()
+        {
+
+        }
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Git World!");
+            Console.WriteLine("Hello Classes World!");
+            Abiturient aa = new Abiturient(300);
+            Console.WriteLine(aa.a);
         }
     }
+ 
 }
